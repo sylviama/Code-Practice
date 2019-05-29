8e6c3494827dd0b84d089470605641fe514cc2ef
Sylvia Ma <xiangwma@umich.edu>
2019-05-29 11:33:21 -0400

    Update FindDuplicates.cs

--- FindDuplicates.cs
+++ FindDuplicates.cs
@@ -6,7 +6,7 @@ public class Program
 	public static void Main()
 	{
 		String input = "tutorial horizon";
-        FindDuplicate.findDuplicates(input);
+        	FindDuplicate.findDuplicates(input);
 	}
 }
 
